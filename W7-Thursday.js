850ac57c834b059a7c5c6fd8d5ff1b30c6af9486
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-04-28 08:51:06 +1200

    Changes before week 8

--- W7-Thursday.js
+++ W7-Thursday.js
@@ -1,11 +1,23 @@
-function createLargeArray() {
-  let largeArray = new Array(1000000);
-  return function(num = 0) {
-    console.log(largeArray.length - num);
-  };
+// function createLargeArray() {
+//   let largeArray = new Array(1000000);
+//   return function(num = 0) {
+//     console.log(largeArray.length - num);
+//   };
+// }
+
+// let printArrayLength = createLargeArray();
+// let printArrayLength2 = createLargeArray();
+// printArrayLength();
+// printArrayLength2(999);
+
+function createCounter(num) {
+    let count = num;
+    return function (num = 1) {
+        count += num;
+        return count;
+    };
 }
 
-let printArrayLength = createLargeArray();
-let printArrayLength2 = createLargeArray();
-printArrayLength();
-printArrayLength2(999);
+let counter = createCounter(5);
+console.log(counter(2)); // 1
+console.log(counter()); // 2
